85f4c926e955dade09113bc3cc3113c7e3f6f6d6
Tiago-salva <maitetiago58@gmail.com>
2024-03-10 17:44:31 -0300

    Added the dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      favicon: "./src/svg/favicon.ico",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+	mode: 'development',
+	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
 };
